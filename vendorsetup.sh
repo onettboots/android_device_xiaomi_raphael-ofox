7bb44bd402448d041018c362094acf74e1026eaa
naveenjohnsonv <14140949+naveenjohnsonv@users.noreply.github.com>
2021-06-08 02:08:43 +0530

    Build unofficial and add some build flags

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,8 +17,6 @@
 # 	
 # 	Please maintain this if you use this script or any part of it
 #
-FDEVICE="raphael"
-if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
         export PLATFORM_VERSION="16.1.0"
    	export PLATFORM_SECURITY_PATCH="2099-12-31"
    	export TW_DEFAULT_LANGUAGE="en"
@@ -36,8 +34,9 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
 	export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI=1
 	export FOX_DELETE_AROMAFM=1
-
+	export OF_DONT_KEEP_LOG_HISTORY=1
 	export OF_FBE_METADATA_MOUNT_IGNORE=1
+
 	export FOX_USE_BASH_SHELL=1
 	export FOX_ASH_IS_BASH=1
 	export FOX_USE_NANO_EDITOR=1
@@ -49,7 +48,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export OF_SKIP_MULTIUSER_FOLDERS_BACKUP=1
         export OF_USE_SYSTEM_FINGERPRINT=1
    	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1510672800"; # Tue Nov 14 15:20:00 GMT 2017
-	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-21.4.zip
+	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk.apk
 
 	# OTA for custom ROMs
         export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
@@ -60,6 +59,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
         export OF_USE_TWRP_SAR_DETECT=1
         export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
         export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;/vendor_image;"
+	export OF_USE_LOCKSCREEN_BUTTON=1
         #export FOX_ADVANCED_SECURITY=1
         # -- end R11 settings --
 
@@ -73,7 +73,5 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
   	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
  	fi
 
-	add_lunch_combo omni_"$FDEVICE"-eng
-	add_lunch_combo omni_"$FDEVICE"-userdebug
-fi
-#
+add_lunch_combo omni_raphael-eng
+add_lunch_combo omni_raphael-userdebug
